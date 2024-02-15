15dac0e8320121fc4cbf73cc4fd65d26afd0c2e4
MertJSX <122701396+MertJSX@users.noreply.github.com>
2024-02-15 21:01:14 +0200

    Improved style

--- src/pages/dev/Dev.jsx
+++ src/pages/dev/Dev.jsx
@@ -1,17 +1,21 @@
-import "./dev.scss"
-import Navbar from "../../components/navbar/Navbar"
+import "./dev.scss";
+import Navbar from "../../components/navbar/Navbar";
 
 const Dev = () => {
   return (
     <div className="devPage">
       <div className="bg-img"></div>
-      <Navbar/>
+      <Navbar />
       <div className="content">
         <h1>За сайта</h1>
-        <h2>Сайтът е направен от Мерт Сами. Използваните технологии са HTML, CSS, JavaScript, NodeJS, ReactJS, ReactRouterDOM и Sass.</h2>
+        <h2>
+          Сайтът е направен от Мерт Сами. Използваните технологии са HTML, CSS,
+          JavaScript, NodeJS, ReactJS, ReactRouterDOM и Sass. Използван е
+          изкуствен интелект за подновяването на някои снимки.{" "}
+        </h2>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dev
+export default Dev;
